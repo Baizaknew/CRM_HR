69a5d088dd03b5ad9615a1756ac256800a9a914c
Baizak <BZhumaev@beeline.kg>
2025-04-24 16:01:37 +0600

    fixed choice class names

--- apps/user/choices.py
+++ apps/user/choices.py
@@ -1,7 +1,7 @@
 from django.db.models import TextChoices
 
 
-class UserRoleChoices(TextChoices):
+class UserRole(TextChoices):
     DEPARTMENT_HEAD = 'DEPARTMENT_LEAD', 'Руководитель'
     HR_LEAD = 'HR_LEAD', 'Глава HR'
     RECRUITER = 'RECRUITER', 'Рекрутер'
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-from apps.user.choices import UserRoleChoices
+from apps.user.choices import UserRole
 
 
 class User(AbstractUser):
-    role = models.CharField("Роль", choices=UserRoleChoices.choices, max_length=30)
+    role = models.CharField("Роль", choices=UserRole.choices, max_length=30)
 
     def __str__(self):
         return self.username
