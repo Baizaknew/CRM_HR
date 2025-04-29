80d5e38d886a45869324f80bf8187f6fe4ec2bb1
Baizak <BZhumaev@beeline.kg>
2025-04-29 08:51:02 +0600

    so far, I've removed the base user admin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -6,6 +6,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 @admin.register(User)
-class UserAdmin(BaseUserAdmin):
+class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'role')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'role')
