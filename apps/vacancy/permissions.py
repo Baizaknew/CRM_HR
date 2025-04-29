e9dc4d2f7ce94cbe65d65e861cc300b968acdf8f
Baizak <BZhumaev@beeline.kg>
2025-04-29 09:05:18 +0600

    added all permissions for vacancy request

--- apps/vacancy/permissions.py
+++ apps/vacancy/permissions.py
@@ -1,10 +1,47 @@
 from rest_framework.permissions import BasePermission
 
 from apps.user.choices import UserRole
+from apps.vacancy.choices import VacancyRequestStatus
 
 
 class IsHrLeadOrDepartmentHead(BasePermission):
+    """Доступ HR главе и руководителю"""
     def has_permission(self, request, view):
-        if request.user.role in (UserRole.HR_LEAD, UserRole.DEPARTMENT_HEAD):
-            return True
-        return False
+        return request.user.is_authenticated and request.user.role in (UserRole.HR_LEAD, UserRole.DEPARTMENT_HEAD)
+
+
+class IsHrLead(BasePermission):
+    """Доступ главе HR"""
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == UserRole.HR_LEAD
+
+
+class IsDepartmentHead(BasePermission):
+    """Доступ руководителю"""
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == UserRole.DEPARTMENT_HEAD
+
+
+class IsOwner(BasePermission):
+    """Доступ владельцу заявки"""
+    def has_object_permission(self, request, view, obj):
+        return obj.requester == request.user and request.user.is_authenticated
+
+
+class CanEditWhenNeedsRevisions(BasePermission):
+    """Доступ руководителю к редактированию, если статус объекта - NEEDS_REVISION"""
+    message = "Руководитель может редактировать, только если статус Заявки - 'Требует доработки'"
+    def has_object_permission(self, request, view, obj):
+        return obj.status == VacancyRequestStatus.NEEDS_REVISION
+
+
+class CanResubmitWhenNeedsRevisions(CanEditWhenNeedsRevisions):
+    """Тоже самое, что и CanEditWhenNeedsRevisions, новый класс для ясности в названии"""
+    pass
+
+
+class CanAdminActOnReview(BasePermission):
+    """Доступ HR на изменение статуса, только если Заявка со статусом - 'На рассмотрении'"""
+    message = "Разрешено если статус Заявки - 'На рассмотрении'"
+    def has_object_permission(self, request, view, obj):
+        return obj.status == VacancyRequestStatus.IN_REVIEW
