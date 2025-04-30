e0147998666f0c0bcb54a9edb636092040e994c2
Baizak <BZhumaev@beeline.kg>
2025-04-30 10:18:19 +0600

    added permissions for vacancy

---
+++ apps/vacancy/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework.permissions import BasePermission
+
+from apps.user.choices import UserRole
+
+
+class IsHrLeadOrAssignedRecruiter(BasePermission):
+    """Доступ для редактирования только своих вакансий"""
+    message = "Вы можете редактировать только свои Вакансии"
+    def has_object_permission(self, request, view, obj):
+        return (request.user.is_authenticated and
+                request.user == obj.recruiter or
+                request.user.role == UserRole.HR_LEAD)
+
+
+class IsRecruiter(BasePermission):
+    """Доступ только рекрутеру"""
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == UserRole.RECRUITER
