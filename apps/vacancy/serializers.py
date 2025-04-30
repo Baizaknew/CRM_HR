4a9a4cd311c8197c13f865dee0d6fc3370fb39d6
Baizak <BZhumaev@beeline.kg>
2025-04-30 10:18:42 +0600

    added serializers for vacancy

---
+++ apps/vacancy/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+
+from apps.vacancy.models import Vacancy
+from apps.vacancy_request.serializers import UserSimpleSerializer
+
+
+class VacancyListSerializerForHRandRecruiter(serializers.ModelSerializer):
+    recruiter = UserSimpleSerializer(read_only=True)
+    department_lead = UserSimpleSerializer(read_only=True)
+    class Meta:
+        model = Vacancy
+        fields = ('id', 'title', 'department', 'city', 'priority', 'created_at', 'updated_at', 'status',
+                  'opened_at', 'closed_at', 'recruiter', 'department_lead', 'time_to_offer')
+
+
+class VacancyListSerializerForDepartmentHead(serializers.ModelSerializer):
+    recruiter = UserSimpleSerializer(read_only=True)
+    department_lead = UserSimpleSerializer(read_only=True)
+    class Meta:
+        model = Vacancy
+        fields = ('id', 'title', 'priority', 'created_at', 'updated_at', 'status',
+                  'opened_at', 'closed_at', 'recruiter', 'department_lead', 'time_to_offer')
+
+
+class VacancyDetailSerializer(serializers.ModelSerializer):
+    recruiter = UserSimpleSerializer(read_only=True)
+    department_lead = UserSimpleSerializer(read_only=True)
+    class Meta:
+        model = Vacancy
+        fields = ('id', 'title', 'department', 'city', 'requirements', 'responsibilities', 'priority', 'created_at',
+                  'updated_at', 'status', 'opened_at', 'closed_at', 'recruiter', 'department_lead', 'time_to_offer',
+                  'salary',)
+
+
+class VacancyUpdateSerializerForHrLead(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = ('id', 'title', 'department', 'city', 'requirements', 'responsibilities', 'priority',
+                  'status', 'recruiter', 'department_lead',
+                  'salary',)
+
+
+class VacancyUpdateSerializerForRecruiter(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = ('id', 'title', 'department', 'city', 'requirements', 'responsibilities',
+                  'status', 'salary',)
+
+
+class VacancyCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = ('title', 'department', 'city', 'requirements', 'responsibilities', 'priority', 'department_lead',
+                  'recruiter', 'vacancy_request')
