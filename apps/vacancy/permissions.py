ec098a86599171c4d804f34bbd554387d936033f
Baizak <BZhumaev@beeline.kg>
2025-05-04 21:38:32 +0600

    returned permission for vacancy and added new  for vacancy owner

--- apps/vacancy/permissions.py
+++ apps/vacancy/permissions.py
@@ -3,14 +3,24 @@ from rest_framework.permissions import BasePermission
 from apps.user.choices import UserRole
 
 
-class IsAssignedRecruiter(BasePermission):
+class IsHrLeadOrAssignedRecruiter(BasePermission):
     """Доступ для редактирования только своих вакансий"""
     message = "Вы можете редактировать только свои Вакансии"
     def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated and request.user == obj.recruiter
+        return (request.user.is_authenticated and
+                request.user == obj.recruiter or
+                request.user.role == UserRole.HR_LEAD)
 
 
 class IsRecruiter(BasePermission):
     """Доступ только рекрутеру"""
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == UserRole.RECRUITER
+
+
+class IsVacancyOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (request.user.is_authenticated and
+                request.user == obj.recruiter or
+                request.user == obj.department_lead or
+                request.user.role == UserRole.HR_LEAD)
