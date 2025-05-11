211bd086cff5097f16d719918f8287af78fad863
Baizak <BZhumaev@beeline.kg>
2025-05-11 20:30:35 +0600

    added api's for user

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from apps.user.views import UserViewSet
+
+
+router = DefaultRouter()
+router.register('', UserViewSet, basename='users')
+
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
+
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
-# Create your views here.
+from apps.user.serializers import UserSerializer
+from apps.vacancy_request.permissions import IsHrLead
+from apps.user.permissoins import IsUser
+
+User = get_user_model()
+
+
+@extend_schema(tags=['user'])
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action in ('create', 'delete', 'update', 'partial_update'):
+            return (IsHrLead(),)
+        elif self.action in ('update', 'partial_update'):
+            return (IsUser(),)
+        return (IsAuthenticated(),)
