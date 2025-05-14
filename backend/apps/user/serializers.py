b1d6bcd92b3b89c708486ad678541334f5343fb5
Baizak <BZhumaev@beeline.kg>
2025-05-14 09:23:59 +0600

    fixed serializer for user

--- backend/apps/user/serializers.py
+++ backend/apps/user/serializers.py
@@ -9,7 +9,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'role', 'password')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'role', 'password')
 
     def create(self, validate_data):
         password = validate_data.pop('password')
