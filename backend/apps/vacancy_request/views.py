25950091c3341f1764c182cd1fcf882dc786db27
Baizak <BZhumaev@beeline.kg>
2025-05-14 09:23:17 +0600

    added perform create

--- backend/apps/vacancy_request/views.py
+++ backend/apps/vacancy_request/views.py
@@ -154,3 +154,6 @@ class VacancyRequestCommentViewSet(viewsets.ModelViewSet):
     ])
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
