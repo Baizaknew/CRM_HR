43778cc95d0197249ba4cf7dac75e7f52a84ff76
Baizak <BZhumaev@beeline.kg>
2025-05-14 09:05:55 +0600

    fixed serializer for create user

--- backend/apps/user/serializers.py
+++ backend/apps/user/serializers.py
@@ -1,3 +1,4 @@
+from wsgiref import validate
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
@@ -9,3 +10,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'role', 'password')
+
+    def create(self, validate_data):
+        password = validate_data.pop('password')
+        user = User(**validate_data)
+        user.set_password(password)
+        user.save()
+        return user
+
